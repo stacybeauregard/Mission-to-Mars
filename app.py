0ffff863ac9a0400ff252e32d617af1eaf3927d2
Stacy <stacy.beauregard@gmail.com>
2022-08-24 22:06:37 -0400

    challenge 10

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ def index():
    return render_template("index.html", mars=mars)
 
    @app.route("/scrape")
-def scrape():
-   mars = mongo.db.mars
+   def scrape():
+      mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
    if __name__ == "__main__":
-   app.run()
+      app.run()
